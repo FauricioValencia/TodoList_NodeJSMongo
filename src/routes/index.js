74bb8f977d896b532f32173543dea08c2f3d36d5
FauricioValencia <julian.f.valencia@hotmail.com>
2018-09-25 01:06:56 -0500

    add service /add, /getTask, /delete

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,12 +1,32 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-router.get('/', (req,res)=>{
-    res.render('index')
-})
+const Task = require("../models/task.js"); //contiene el esquema de los datos
 
-router.post('/add', (req,res)=>{
-    console.log(req.body);
-    res.send('ok') 
+router.get("/", async (req, res) => {
+  const tasks = await Task.find();
+  console.log(tasks);
+  res.render("index",{
+      tasks
+  });
+});
+
+router.post("/add", (req, res) => {
+  const task = new Task(req.body);
+  task
+    .save()
+    .then(res => {
+      console.log(res);
+    })
+    .catch(e => {
+      console.log("error", e);
+    });
+  res.redirect("/");
+});
+router.get("/delete/:id",async(req,res)=>{
+    const {id} =req.params;
+    await Task.remove({_id:id})
+    res.redirect("/");
+
 })
 module.exports = router;
